96f965bb634c3d4857573b5d52f650b709908b91
izzee <ichabon@me.com>
2018-01-31 17:33:44 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,40 @@ require 'pry'
 class String
 
   def sentence?
-    
+
+    if self[-1] == "."
+      true
+    else
+      false
+    end
+
   end
 
   def question?
+
+    if self[-1] == "?"
+      true
+    else
+      false
+    end
 
   end
 
   def exclamation?
 
+    if self[-1] == "!"
+      true
+    else
+      false
+    end
+
   end
 
   def count_sentences
-
+    num_sentances = self.split(' ').select do |w|
+      w[-1] == '.' || w[-1] == '?' || w[-1] == '!'
+    end
+    num_sentances.length
+    # binding.pry
   end
 end
